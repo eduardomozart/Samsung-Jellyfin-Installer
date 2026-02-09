64d610d6b2c02a19d5c00f4a60f9a64f808479a5
PatrickSt1991 <patrick.stel@kpnmail.nl>
2026-02-09 10:26:42 +0100

    Different location for Linux

--- Jellyfin2Samsung-CrossOS/Program.cs
+++ Jellyfin2Samsung-CrossOS/Program.cs
@@ -3,6 +3,7 @@ using Jellyfin2Samsung.Extensions;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Jellyfin2Samsung
 {
@@ -11,16 +12,26 @@ namespace Jellyfin2Samsung
         [STAThread]
         public static void Main(string[] args)
         {
-            // Register trace listener BEFORE Avalonia starts
-            var logDir = AppContext.BaseDirectory;
-            string logFolder = Path.Combine(logDir, "Logs");
+            string logFolder;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                var logRoot = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                logFolder = Path.Combine(logRoot, "Jellyfin2Samsung", "Logs");
+            }
+            else
+            {
+                // Windows/macOS: keep existing behavior
+                logFolder = Path.Combine(AppContext.BaseDirectory, "Logs");
+            }
+
             Directory.CreateDirectory(logFolder);
+
             var dtg = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss-fff");
             var logFile = Path.Combine(logFolder, $"debug_{dtg}.log");
 
             Trace.Listeners.Add(new FileTraceListener(logFile));
             Trace.AutoFlush = true;
-
 
             BuildAvaloniaApp()
                 .StartWithClassicDesktopLifetime(args);
